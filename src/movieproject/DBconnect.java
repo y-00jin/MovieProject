9bef7523d8c90372722b2150fee2542a791622ed
Yoojin <81798918+y-00jin@users.noreply.github.com>
2022-11-09 16:56:46 +0900

    Update DBconnect.java

--- src/movieproject/DBconnect.java
+++ src/movieproject/DBconnect.java
@@ -11,8 +11,7 @@ public class DBconnect {
 	public static void DB() {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			 conn = DriverManager.getConnection("jdbc:oracle:thin:@114.71.137.174:53994:XE", "JAVAJO", "6666");
-//			 conn = DriverManager.getConnection("jdbc:oracle:thin:@114.71.137.174:53994:XE", "jagijang", "5555");
+			 conn = DriverManager.getConnection("jdbc:oracle:thin:@IP주소:XE", "ID", "PW");
 			 //System.out.println("연동");
 			 stat = conn.createStatement();
 			
